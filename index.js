776bb8b7d4b2641caddbf86c9a27352552eb5831
Mauricio Fuentes <mauriciofb@gmail.com>
2024-06-17 20:05:35 -0400

    Views skater API

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import morgan from 'morgan'
 import fileUpload from 'express-fileupload'
 import { engine } from 'express-handlebars'
+// importar rutas
+import { router as skaters } from './routes/skaters.js'
+import { router as views } from './routes/views.js'
+import { router as auth } from './routes/auth.js'
 
 // Instancia servidor
 
@@ -18,6 +22,11 @@ app.use(express.static('static'))
 // Motor de vistas
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+
+// registrar rutas
+app.use("/", views)
+app.use("/skaters", skaters)
+app.use("/auth", auth)
 
 
 app.listen(3000, () => {console.log("App escuchando el puerto 3000")})
---
+++ routes/views.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { createSkater, getSkaters, updateSkater, deleteStaker, getSkater } from "../models/skaters.js";
+import jwt from 'jsonwebtoken'
+import path from 'node:path'
+
+const router  = Router()
+
+// Admin
+router.get("/admin", async (req, res) => {
+  const skaters = await getSkaters()
+  res.render("admin", {
+    skaters: skaters.rows
+  })
+})
+// Login
+router.get("/login", async(req, res) => {
+  res.render("login")
+})
+
+// registro
+router.get("/registro", async (req, res) => {
+  res.render("registro")
+})
+
+// Datos
+router.get("/datos", async (req, res) => {
+  res.render("datos")
+})
+// Home
+router.get("/", async (req, res) => {
+  const skaters = await getSkaters()
+  res.render("home", {
+    skaters: skaters.rows
+  })
+})
+
+export { router }
+
+
+
